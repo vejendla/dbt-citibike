47a82e5482341f518d44761510e1670bc89b8ace
nithin.vejendla@gmail.com <nithin.vejendla@gmail.com>
2023-05-29 20:22:54 +0000

    created intermediate model

--- models/intermediate/int_routes_with_coordinates.sql
+++ models/intermediate/int_routes_with_coordinates.sql
@@ -5,6 +5,14 @@ with trips as (
 final as (
     SELECT 
         CONCAT(trips.start_station_name, " to ", trips.end_station_name) as route,
+        start_station_id,
+        start_station_name,
+        start_station_latitude,
+        start_station_longitude,
+        end_station_id,
+        end_station_name,
+        end_station_latitude,
+        end_station_longitude,
         COUNT(*) as num_trips,
         ROUND(AVG(cast(trips.tripduration as int64) / 60),2) as duration
     FROM 
